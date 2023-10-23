034ffccabfab933fa3f44ea874eac03caf3867c7
ghailen <ghailenemark@gmail.com>
2023-10-23 22:26:02 +0200

    pushing

--- currency-exchange-service/src/main/java/com/ghailene/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/ghailene/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -36,6 +36,7 @@ public class CurrencyExchangeController {
         }
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
+        logger.info("hahhahahaaahaaahhahahahahaaha aaaaaaaaaaaaaaaaaaaa");
         return currencyExchange;
 
     }

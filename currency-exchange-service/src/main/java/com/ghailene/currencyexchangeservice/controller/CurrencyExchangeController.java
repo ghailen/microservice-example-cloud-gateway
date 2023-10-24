3888dec5efd016be3d6b648b59da50c11cf923da
ghailen <ghailenemark@gmail.com>
2023-10-24 16:59:44 +0200

    updating loggg

--- currency-exchange-service/src/main/java/com/ghailene/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/ghailene/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -36,7 +36,7 @@ public class CurrencyExchangeController {
         }
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
-        logger.info("hahhahaheaaaheeaaaahheahahahahjrrahajaahaahahasalaaaa");
+        logger.info("hahhahaheaaaheeaaaahheahahahahjrrahajaahaahaheasalaaaa");
         return currencyExchange;
 
     }
